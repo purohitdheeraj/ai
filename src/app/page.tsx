b546ed93b684bcf1bd8325942854c3349cb8066e
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-11-29 13:25:20 +0530

    meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import  Navbar from "@/components/navbar"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hare Krishna
-      <Button>Click me</Button>
+    <div>
+    <Navbar/>
+    <main>Main</main>
+    <footer>Footer</footer>
     </div>
   );
 }
